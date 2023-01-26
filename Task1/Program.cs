353aa0bfd1467ade92cd7738d7a56e645eaffb3c
Svetlana Burlakova <svetlya4ok83@gmail.com>
2023-01-26 12:42:35 +0400

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число 1");
+string number1Str=Console.ReadLine();
+int number1=Convert.ToInt32(number1Str);
+Console.WriteLine("Введите число 2");
+string number2Str=Console.ReadLine();
+int number2=Convert.ToInt32(number2Str);
+if (number1 > number2)
+Console.WriteLine($"число {number1} больше, чем число {number2}");
+else
+Console.WriteLine($"число {number2} больше, чем число {number1}");
