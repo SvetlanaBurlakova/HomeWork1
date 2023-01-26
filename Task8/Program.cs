b1b6e2ffb836a517bf52b4692a090bffc7b65b20
Svetlana Burlakova <svetlya4ok83@gmail.com>
2023-01-26 13:11:11 +0400

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+string numberStr=Console.ReadLine();
+int number=Convert.ToInt32(numberStr);
+int index=2;
+if (number>=2)
+{while (index<=number)
+    {
+        Console.Write($"{index}, ");
+        index+=2;
+    }
+}
+else
+Console.WriteLine("четных чисел в этом диапазоне нет");
