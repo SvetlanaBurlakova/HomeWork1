c48db437903e6d28a9fdd98d0109fbb20be4ad9b
Svetlana Burlakova <svetlya4ok83@gmail.com>
2023-01-26 12:59:26 +0400

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+string numberStr=Console.ReadLine();
+int number=Convert.ToInt32(numberStr);
+if (number%2==0)
+Console.WriteLine($"число {number} четное");
+else
+Console.WriteLine($"число {number} нечетное");  
